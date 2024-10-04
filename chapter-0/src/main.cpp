ca740c6f82b2f9e32eb23723c657796fcfbb648c
Logan <pix.59700@gmail.com>
2024-10-04 11:53:35 +0200

    Adding Methode for display position in main

--- chapter-0/src/main.cpp
+++ chapter-0/src/main.cpp
@@ -2,10 +2,19 @@
 
 #include "Point2D.hpp"
 
+void DisplayPoint2D(Point2D point2D) {
+    std::cout << "Abscissa: " << point2D.Abscissa() << " Ordinate: " << point2D.Ordinate() << std::endl;
+}
+
 int main()
 {
     Point2D point2D = Point2D(2, 4);
+
+    DisplayPoint2D(point2D);
+
     point2D.Translate(2, 4);
+
+    DisplayPoint2D(point2D);
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
