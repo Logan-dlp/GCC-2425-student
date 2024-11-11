7b7cf3c286090057ecdc7d47eaf7156a600f7148
Logan <pix.59700@gmail.com>
2024-11-11 15:17:45 +0100

    add to translate all point

--- chapter-0/src/main.cpp
+++ chapter-0/src/main.cpp
@@ -4,6 +4,12 @@
 #include "WeightedPoint.hpp"
 #include "ColouredPoint.hpp"
 
+void TranslateAll(Point2D** points, int size, float dx, float dy) {
+    for (int i = 0; i < size; ++i) {
+        points[i]->Translate(dx, dy);
+    }
+}
+
 void DisplayPoint(const Point2D& point2D){
     std::cout << point2D.ToString() << std::endl;
 }
@@ -13,6 +19,10 @@ int main()
     Point2D p1 = Point2D(1, 5);
     WeightedPoint wp1 = WeightedPoint(1, 3, 2);
     ColouredPoint cp1 = ColouredPoint(3, 4, 255);
+
+    Point2D* points[] = {&p1, &wp1, &cp1};
+
+    TranslateAll(points, std::size(points), 2, 3);
 
     DisplayPoint(p1);
     DisplayPoint(wp1);
